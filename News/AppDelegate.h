3dd2b4d63d368505c72921333100f208d16eda1b
xiaoyuan <392237716@qq.com>
2018-01-17 13:10:49 +0800

    添加今日头条下百万英雄Tweak项目

---
+++ News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by xiaoyuan on 2018/1/17.
+//  Copyright © 2018年 alpface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  News
+//
+//  Created by xiaoyuan on 2018/1/17.
+//  Copyright © 2018年 alpface. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
